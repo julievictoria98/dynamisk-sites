68e1b626570883b927ae768d864c314fd2ab85c9
julievictoria98 <jura0002@stud.kea.dk>
2024-02-04 11:24:10 +0100

    ny css-side, begyndt på js

--- js.js
+++ js.js
@@ -1,3 +1,50 @@
+
+fetch("https://kea-alt-del.dk/t7/api/products")
+.then(res =>res.json()).then(data=>showProducts(data))
+
+function showProducts(products){
+
+    products.forEach(showProduct);
+
+}
+
+function showProduct(product){
+    console.log("product")
+    const template = document.querySelector("#productlist_template").content;
+    const clone = template.cloneNode(true);
+
+    clone.querySelector(".productname").textContent = product.productdisplayname;
+
+    document.querySelector("main").appendChild(clone);
+
+}
+
+/* [{"id":1163,
+"gender":"Men",
+"category":"Apparel",
+"subcategory":"Topwear",
+"articletype":"Tshirts",
+"season":"Summer",
+"productionyear":2011,
+"usagetype":"Sports",
+"productdisplayname":"Sahara Team India Fanwear Round Neck Jersey",
+"price":895,
+"discount":null,
+"brandname":"Nike","
+soldout":0} */
+
+
+
+
+
+
+
+
+
+
+
+/*Soldout funtion */
+
 "use strict"
 
 window.addEventListener('load', main);

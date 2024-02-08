782b4e9251afd747e1377744fdce7050f143795c
julievictoria98 <jura0002@stud.kea.dk>
2024-02-08 11:34:44 +0100

    rettelse

--- js/category.js
+++ js/category.js
@@ -2,22 +2,16 @@ fetch("https://kea-alt-del.dk/t7/api/categories")
 .then(res =>res.json()).then(data=>showCats(data))
 
 function showCats(cats){
-
 cats.forEach(showCat);
-
-
 }
 
 function showCat(cat){
     console.log("cat")
-
     const template = document.querySelector("template").content;
     const clone = template.cloneNode(true);
     
     clone.querySelector("h2").textContent = cat.category
     clone.querySelector("a").href = `productlist.html?category=${cat.category} `
     document.querySelector("article").appendChild(clone);
-
-   
 
 }
